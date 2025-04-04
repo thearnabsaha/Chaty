637d24bc390d875fa04f06bf6aadba01729b1f7b
thearnabsaha <thearnabsaha201@gmail.com>
2025-04-04 14:27:16 +0530

    feat: implement room creation and dynamic room code generation in CreateRoom component

--- chaty-client/src/pages/CreateRoom.tsx
+++ chaty-client/src/pages/CreateRoom.tsx
@@ -7,12 +7,30 @@ import { BsChat, BsCheck } from "react-icons/bs";
 import { MdOutlineContentCopy } from "react-icons/md";
 const CreateRoom = () => {
     const [copied, setCopied] = useState<Boolean>(false)
+    const [roomCreated, setRoomCreated] = useState<Boolean>(false)
+    const [roomId, setRoomId] = useState("0354EC9")
+    const [inputValue, setInputValue] = useState("")
     const handleCopy=()=>{
-        navigator.clipboard.writeText("0354EC9")
+        navigator.clipboard.writeText(roomId)
         setCopied(true)
         setTimeout(() => {
             setCopied(false)
         }, 1000);
+    }
+    const generateRoomCode = () => {
+        const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        let code = '';
+        for (let i = 0; i < 6; i++) {
+          code += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+        return code;
+      };
+    const handleCreateRoom=()=>{
+        setRoomId(generateRoomCode())
+        setRoomCreated(true)
+    }
+    const handleJoin=()=>{
+        alert(inputValue)
     }
   return (
     <div className=' w-screen h-screen flex justify-center items-center font-mono'>
@@ -30,27 +48,29 @@ const CreateRoom = () => {
           </CardDescription>
         </CardContent>
         <CardHeader>
-          <Button className='rounded-sm cursor-pointer py-5 font-bold'>Create New Room</Button>
+          <Button className='rounded-sm cursor-pointer py-5 font-bold' onClick={handleCreateRoom}>Create New Room</Button>
         </CardHeader>
         <CardFooter>
-            <Input className='rounded-sm mr-2 font-semibold' placeholder='Enter Room Code'/>
-            <Button className='rounded-sm cursor-pointer font-semibold'>Join Room</Button>
+            <Input className='rounded-sm mr-2 font-semibold' placeholder='Enter Room Code' value={inputValue} onChange={(e)=>setInputValue(e.target.value)}/>
+            <Button className='rounded-sm cursor-pointer font-semibold' disabled={!inputValue} onClick={handleJoin}>Join Room</Button>
         </CardFooter>
-        <CardContent>
+        {
+            roomCreated&&<CardContent>
             <div className="bg-accent py-10 rounded-sm">
                 <CardDescription>
                     <p className="text-shadow-accent-foreground font-bold text-center mb-5">Share This Code With Your Friend</p>
                 </CardDescription>
                 <div className="flex justify-center">
-                    <h1 className="text-center font-bold text-3xl mr-10">0354EC9</h1>
+                    <h1 className="text-center font-bold text-3xl mr-5">{roomId}</h1>
                     
-                    <Button className=" rounded-sm cursor-pointer duration-500 h-10 w-10" onClick={handleCopy}>
+                    <Button className=" rounded-sm cursor-pointer duration-500 h-10 w-10 -translate-y-0.5" onClick={handleCopy}>
                     {copied?<BsCheck className="text-3xl"/> 
                         :<MdOutlineContentCopy className="text-xl"/>}
                     </Button>
                 </div>
             </div>
         </CardContent>
+        }
       </Card>
     </div>
   )
