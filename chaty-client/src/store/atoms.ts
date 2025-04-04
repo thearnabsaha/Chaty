292f7af2326dd94eb81861dd2af32085d84b4219
thearnabsaha <thearnabsaha201@gmail.com>
2025-04-04 15:21:42 +0530

    feat: update default values for state atoms in Recoil store

--- chaty-client/src/store/atoms.ts
+++ chaty-client/src/store/atoms.ts
@@ -1,21 +1,21 @@
 import { atom } from "recoil";
 export const copiedState = atom({
-    key: 'copied', // unique ID (with respect to other atoms/selectors)
-    default: false, // default value (aka initial value)
+    key: 'copied',
+    default: false,
   });
 export const roomCreatedState = atom({
-    key: 'roomCreated', // unique ID (with respect to other atoms/selectors)
-    default: false, // default value (aka initial value)
+    key: 'roomCreated',
+    default: false,
   });
 export const joinedState = atom({
-    key: 'joined', // unique ID (with respect to other atoms/selectors)
-    default: true, // default value (aka initial value)
+    key: 'joined',
+    default: false,
   });
 export const roomIdState = atom({
-    key: 'roomId', // unique ID (with respect to other atoms/selectors)
-    default: "", // default value (aka initial value)
+    key: 'roomId',
+    default: "",
   });
 export const inputValueState = atom({
-    key: 'inputValue', // unique ID (with respect to other atoms/selectors)
-    default: "", // default value (aka initial value)
+    key: 'inputValue',
+    default: "",
   });
