085e2e9a8eb838a9d8f5a6e6f809be54e1c08499
thearnabsaha <thearnabsaha201@gmail.com>
2025-04-04 15:32:26 +0530

    feat: add Textarea component and update Room to use it for message input

--- chaty-client/src/pages/Room.tsx
+++ chaty-client/src/pages/Room.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button"
 import { CardContent, CardDescription, CardFooter } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
 import { roomIdState } from "@/store/atoms"
 import { useState } from "react"
 import { useRecoilState } from "recoil"
@@ -8,11 +9,18 @@ import { useRecoilState } from "recoil"
 const Room = () => {
     const [roomId, _setRoomId] = useRecoilState(roomIdState)
     const [inputValue, setInputValue] = useState("")
-    const handleKeydown=(e:React.KeyboardEvent<HTMLInputElement>)=>{
-        if(e.key=="Enter"){
-            setInputValue("")
-        }
+    const handleKeydown=(e:React.KeyboardEvent<HTMLTextAreaElement>)=>{
+        // if(e.key=="Enter"){
+        //     setInputValue("")
+        // }
+        if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault(); // Stop new line
+            console.log('Sending:', inputValue); // Replace with your logic
+            setInputValue('');
+          }
+      
     }
+    
   return (
     <div>
         <CardContent>
@@ -22,9 +30,9 @@ const Room = () => {
                     <p>Users : 3/5</p>
                 </CardDescription>
                 </div>
-                <div className=" w-full h-[500px] border rounded-sm mt-2"></div>
-                <div className="my-2 flex justify-between">
-                    <Input className='rounded-sm mr-2 font-semibold focus:border-0' placeholder='Type a message...' value={inputValue} onChange={(e)=>setInputValue(e.target.value)} onKeyDown={handleKeydown}/>
+                <div className=" w-full h-[500px] border rounded-sm mt-4"></div>
+                <div className="my-2 flex justify-between mt-5">
+                    <Textarea className='rounded-sm mr-2 font-semibold focus:border-0 overflow-scroll h-[37px] resize-none ' placeholder='Type a message...' value={inputValue} onChange={(e)=>setInputValue(e.target.value)} onKeyDown={handleKeydown}/>
                     <Button className='rounded-sm cursor-pointer font-semibold' disabled={!inputValue}>Send</Button>
                 </div>
         </CardContent>
