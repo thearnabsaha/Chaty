16e427a3f222b92af4f676b1d6ffed09de6af3f1
thearnabsaha <thearnabsaha201@gmail.com>
2025-04-11 00:00:37 +0530

    feat: establish WebSocket connection in Room component and update message sending logic

--- chaty-client/src/main.tsx
+++ chaty-client/src/main.tsx
@@ -4,9 +4,9 @@ import './index.css'
 import App from './App.tsx'
 import {RecoilRoot} from 'recoil';
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  // <StrictMode>
     <RecoilRoot>
       <App />
     </RecoilRoot>
-  </StrictMode>,
+  // </StrictMode>,
 )
--- chaty-client/src/pages/Room.tsx
+++ chaty-client/src/pages/Room.tsx
@@ -1,26 +1,36 @@
 import { Button } from "@/components/ui/button"
-import { CardContent, CardDescription, CardFooter } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
+import { CardContent, CardDescription } from "@/components/ui/card"
 import { Textarea } from "@/components/ui/textarea"
 import { roomIdState } from "@/store/atoms"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useRecoilState } from "recoil"
 
 const Room = () => {
     const [roomId, _setRoomId] = useRecoilState(roomIdState)
     const [inputValue, setInputValue] = useState("")
+    const [socket, setsocket] = useState<WebSocket|null>(null)
+    useEffect(() => {
+      const newsocket= new WebSocket("ws://localhost:8080")
+      setsocket(newsocket)
+      newsocket.onopen=()=>{
+        console.log("connection established");
+      }
+      newsocket.onmessage=(e)=>{
+        console.log("arnab : "+e.data);
+      }
+    }, [])
     const handleKeydown=(e:React.KeyboardEvent<HTMLTextAreaElement>)=>{
-        // if(e.key=="Enter"){
-        //     setInputValue("")
-        // }
         if (e.key === 'Enter' && !e.shiftKey) {
-            e.preventDefault(); // Stop new line
-            console.log('Sending:', inputValue); // Replace with your logic
+            e.preventDefault();
+            console.log(inputValue)
+            socket?.send(inputValue)
             setInputValue('');
-          }
+        }
       
     }
-    
+    const hadnleSend=()=>{
+        socket?.send(inputValue)
+    }
   return (
     <div>
         <CardContent>
@@ -33,7 +43,7 @@ const Room = () => {
                 <div className=" w-full h-[500px] border rounded-sm mt-4"></div>
                 <div className="my-2 flex justify-between mt-5">
                     <Textarea className='rounded-sm mr-2 font-semibold focus:border-0 overflow-scroll h-[37px] resize-none ' placeholder='Type a message...' value={inputValue} onChange={(e)=>setInputValue(e.target.value)} onKeyDown={handleKeydown}/>
-                    <Button className='rounded-sm cursor-pointer font-semibold' disabled={!inputValue}>Send</Button>
+                    <Button className='rounded-sm cursor-pointer font-semibold' disabled={!inputValue} onClick={hadnleSend}>Send</Button>
                 </div>
         </CardContent>
     </div>
